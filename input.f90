7db1ec40ef435a25b04b87951afb426778e9cd64
Leanstix <leanstixx@gmail.com>
2023-09-25 19:32:38 +0100

    input

--- input.f90
+++ input.f90
@@ -1,7 +1,8 @@
  program input
       implicit none
       character *20 :: name
+      character (len = 15 ) :: fname, lname
       print *, "Hello what's your name"
-      read *, name
-      print *, "your name is", name
+      read *, fname, lname
+      print *, "your name is ", trim(fname) , " " , trim(lname)
 end program input
