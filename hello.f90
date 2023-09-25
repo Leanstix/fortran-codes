d3a231c91ed5070ade69b910d569438ab9dd06a6
Leanstix <leanstixx@gmail.com>
2023-09-25 18:40:59 +0100

    update

--- hello.f90
+++ hello.f90
@@ -1,4 +1,6 @@
 program hello
-      implicit none
+      implicit none !The implicit none statement though not a must is quite important in fortran programming as it is used to remove
+      ! a old feature of fortran which by default treats all variabes that start with the letters i,j,k,l,m,n as "integers" and all
+      ! other variables as real arguments.  
       print *, "hello world"
       end program hello
